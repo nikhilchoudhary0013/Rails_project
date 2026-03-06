4dfe56575ac4158767b098c406d644f39da57739
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2026-03-06 16:11:25 +0530

    Add new column ,migration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
   before_action :set_product, only: %i[ show edit update ]
+
 
   def index
     @products = Product.all
@@ -33,7 +35,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    # @product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
@@ -44,6 +46,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :address ,:description , :featured_image, :inventory_count ])
     end
 end
