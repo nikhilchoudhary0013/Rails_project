d46908a64b05bbd7b322b9606ca1eb7eaa2b1ba6
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2026-03-10 14:57:22 +0530

    add subscribers

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,5 @@
 class Subscriber < ApplicationRecord
   belongs_to :product
+  generates_token_for :unsubscribe
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,11 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
 
+  resources :products do 
+  resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
