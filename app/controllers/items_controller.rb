e8d6ba94b68b4d9ae8715fe3b7ac36b2199ce07f
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2026-04-10 16:01:33 +0530

    Create index ,new ,show and update methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,30 @@ class ItemsController < ApplicationController
    @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end    
+  
+  def create
+    @item = Item.new(item_params)
+    
+    if @item.save
+      redirect_to @item
+    else
+      render :edit
+    end
+  end
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to(@item)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
